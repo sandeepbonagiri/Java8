a0a8cffe76f4673a617a0608cd451952cf6f8c00
unknown <sundeep.bonagiri@gmail.com>
2019-12-25 14:10:37 +0530

    mergered

---
+++ src/main/java/package1/Class1.java
@@ -0,0 +1,18 @@
+package package1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+
+		
+		GreetingService s = (name)->System.out.println("Hi"+name);
+	
+	    s.greet("vinoth");
+	    s.greet("sandeep");
+	    GreetingService d = (actual)->System.out.println("Hello"+ " " +actual.length());
+	    d.greet("Sandeep");
+		
+	}
+}
+
+
---
+++ src/main/java/package1/GreetingService.java
@@ -0,0 +1,7 @@
+package package1;
+
+public interface GreetingService {
+
+	void greet(String message);
+
+}
