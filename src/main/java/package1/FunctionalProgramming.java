49b5248c1490b63283fced3071c61fcf78c55107
Admin <sundeep.bonagiri@gmail.com>
2020-07-25 22:44:24 +0530

    added new chnages

--- src/main/java/package1/FunctionalProgramming.java
+++ src/main/java/package1/FunctionalProgramming.java
@@ -26,6 +26,9 @@ public class FunctionalProgramming {
 				 System.out.println(true);
 				 System.out.println(parts[0]);
 				 System.out.println(str2[0]);
+				 //added new code
+				 System.out.println(str2[1]);
+				 System.out.println(str2[1]);
 
 			  
 			  }
